c9bf1ed6c5a7bf1707585ceece5e79ca3a2fbf66
mol1986@naver.com <mol1986@naver.com>
2022-05-11 19:43:57 +0900

    Stream Input and Output

--- Solution.cpp
+++ Solution.cpp
@@ -10,14 +10,13 @@ int main()
 	// 문자열 입력 함수
 	gets_s(cptr, sizeof(cptr));
 
-
 	// float 변수는 소수점 6 자릿까지 정확도를 표시합니다.
 	float variable = 6.375;
 
 	// double 변수는 소수점 15 자릿까지 정확도를 표시합니다.
 	double x = 1.11111111111111;
  
-	//std::cin >> value;
+	std::cin >> value;
 
 	std::cout << value << std::endl;
 	std::cout << variable << std::endl;
