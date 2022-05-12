a1078f8a0eec094b1c655033742860a38948d0fb
mol1986@naver.com <mol1986@naver.com>
2022-05-12 19:47:40 +0900

    Object Oriented Programing

--- Solution.cpp
+++ Solution.cpp
@@ -1,27 +1,101 @@
 #include <iostream>
+#include <windows.h>
+#include <conio.h>
+
+// class는 접근 지정자를 따로 설정하지 않으면 Private으로 설정됩니다.
+class Monster 
+{ 
+	// 공개 
+public :
+	int x;
+	int y;
+	int z;
+
+	void Death()
+	{
+		HP -= 10;
+		std::cout << HP << std::endl;
+	}
+
+	// 보호
+protected :
+	int money = 100;
+
+	// 비공개
+private :
+
+	int HP = 100;
+
+	void Attack()
+	{
+		std::cout << "Attack" << std::endl;
+	}
+};
+
+class Child_Monster : public Monster
+{
+
+public:
+	void Speaking()
+	{
+		std::cout << "Hello ~" << std::endl;
+		std::cout << money << std::endl;
+	}
+};
+
+// struct는 접근 지정자를 따로 설정하지 않으면 public으로 설정됩니다.
+struct Object
+{	
+	int x = 0; // 멤버 변수
+	int y = 0;
+	int z = 0;
+
+	int HP = 100;
+
+	void Attack() // 멤버 함수
+	{
+		std::cout << "Attack" << std::endl;
+	}
+
+	void Health()
+	{
+		std::cout << "current HP : " << HP << std::endl;
+	}
+};
+
 
 int main()
 {
-	int value = 100;
-	char character = 'A';
+    Object cube;
+	Monster orc;
 
-	char cptr[10];
+	std::cout << cube.x << std::endl;
+	std::cout << cube.y << std::endl;
+	std::cout << cube.z << std::endl;
 
-	// 문자열 입력 함수
-	gets_s(cptr, sizeof(cptr));
+	while (1)
+	{
+		/*
+		if (GetAsyncKeyState(VK_SPACE))
+		{
+			cube.HP -= 10;
+			cube.Health();
+			Sleep(100);
+		}
 
-	// float 변수는 소수점 6 자릿까지 정확도를 표시합니다.
-	float variable = 6.375;
+		if (cube.HP <= 0)
+		{
+			break;
+		}
+		*/
 
-	// double 변수는 소수점 15 자릿까지 정확도를 표시합니다.
-	double x = 1.11111111111111;
- 
-	std::cin >> value;
+		if (GetAsyncKeyState(VK_SPACE))
+		{
+			orc.Death();
+			Sleep(100);
+		}
 
-	std::cout << value << std::endl;
-	std::cout << variable << std::endl;
-	std::cout << x << std::endl;
+	}
 
-	std::cout << character;
 }
 
