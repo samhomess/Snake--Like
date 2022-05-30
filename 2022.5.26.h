6731f96056481ecd6d07358d53c84ff90b61136e
mol1986@naver.com <kileab@naver.com>
2022-05-30 19:46:08 +0900

    Function Overloading

--- 2022.5.26.h
+++ 2022.5.26.h
@@ -99,3 +99,82 @@ int main()
 	return 0;
 }
 */
+
+/*
+#include <iostream>
+#include <string>
+#include <map>
+
+int main()
+{
+	// 연관 컨테이너
+
+	// 노드 기반의 컨테이너 구조입니다.
+
+	// int value = 10;
+	// [key] [value]
+
+	// name          price
+	// [sword] ----- [1000] map : 값은 중복을 허용합니다.
+	// [armor] ----- [300]
+	// [Gloves] ----- [1000]
+	// [sword] x <- map은 key 값에 중복이 허용되지 않습니다.
+
+	// map은 어떤 구조로 되어 있나요?
+	// 레드 블랙 트리 구조로 되어 있습니다.
+	// 레드 블랙 트리 : 자가 균형 이진 탐색 트리로써 삽입과 삭제가 일어나는 경우 자동으로 그 높이를 작게 유지하는 이유는 연산 과정에서 트리의 높이가 한쪽으로 치우치는 것을 방지하기 위해서 입니다.
+
+	// 게임 내에 특정한 값을 넣기 위해서는 map 저장되어 있는 key으로 검색해주시면 됩니다.
+
+
+	// map
+
+			// Key ---- Value
+	std::map<std::string, int> data;
+
+					   // 아이템 이름 <-> 아이템의 가격
+	data.insert(std::make_pair("sword", 200));
+	data.insert(std::make_pair("armor", 500));
+	data.insert(std::make_pair("shoes", 750));
+
+	for (auto iter = data.begin(); iter != data.end(); iter++)
+	{
+		std::cout << "Key : " << iter->first << std::endl;
+		std::cout << "Value : " << iter->second << std::endl;
+	}
+
+	std::cout << "검색한 Key의 값 : " << data.find("shoes")->second << std::endl;
+	data.erase("shoes");
+
+	for (auto iter = data.begin(); iter != data.end(); iter++)
+	{
+		std::cout << "Key : " << iter->first << std::endl;
+		std::cout << "Value : " << iter->second << std::endl;
+	}
+
+	// 알파벳 갯수
+	// 각 알파벳이 단어에 몇 개가 포함되어 있는지 구하는 프로그램입니다.
+
+	std::string value;
+
+	std::getline(std::cin, value);
+
+	int result = 0;
+
+	for (int i = 97; i <= 122; i++)
+	{
+		for (int j = 0; j < value.length(); j++)
+		{
+			if (value[j] == (char)i)
+			{
+				result++;
+			}
+		}
+
+		std::cout << result << std::endl;
+		result = 0;
+	}
+
+
+	return 0;
+}*/
--- Solution.cpp
+++ Solution.cpp
@@ -1,83 +1,136 @@
 #include <iostream>
+#include <set>
 #include <string>
-#include <map>
+
+void Function(int x)
+{
+	std::cout << x << std::endl;
+}
+
+void Function(int x, int y)
+{
+	std::cout << x << "," << y << std::endl;
+}
+
+void Function(char x)
+{
+	std::cout << x << std::endl;
+}
+
+// 반환형으로 함수의 오버로딩을 구현할 수 없습니다.
+/*
+int Function(double x)
+{
+	return x;
+}
+*/
+
+class Phone 
+{
+public:
+	Phone()
+	{
+		std::cout << "따르릉~" << std::endl;
+	}
+
+	Phone(int x, int y)
+	{
+		call = x;
+		size = y;
+
+		std::cout << call << std::endl;
+		std::cout << size << std::endl;
+	}
+
+private :
+	int call;
+	int size;
+};
 
 int main()
-{   
-	// 연관 컨테이너
-    /*
-	// 노드 기반의 컨테이너 구조입니다.
-	
-	// int value = 10;
-	// [key] [value] 
-	
-	// name          price
-	// [sword] ----- [1000] map : 값은 중복을 허용합니다.
-	// [armor] ----- [300]
-	// [Gloves] ----- [1000]
-	// [sword] x <- map은 key 값에 중복이 허용되지 않습니다.
-
-	// map은 어떤 구조로 되어 있나요?
-	// 레드 블랙 트리 구조로 되어 있습니다.
-	// 레드 블랙 트리 : 자가 균형 이진 탐색 트리로써 삽입과 삭제가 일어나는 경우 자동으로 그 높이를 작게 유지하는 이유는 연산 과정에서 트리의 높이가 한쪽으로 치우치는 것을 방지하기 위해서 입니다.
-
-	// 게임 내에 특정한 값을 넣기 위해서는 map 저장되어 있는 key으로 검색해주시면 됩니다.
-	*/
-
-	// map
+{
+	// set
 	/*
-	        // Key ---- Value
-	std::map<std::string, int> data;
+	// std::map -> key와 value 
+	// key을 활용해서 데이터를 확인합니다.
+	
+	// set -> key
+	// key값이 중복이 허용되지 않습니다.
+	// insert 원소가 자동으로 정렬됩니다.
 
-	                   // 아이템 이름 <-> 아이템의 가격
-	data.insert(std::make_pair("sword", 200));
-	data.insert(std::make_pair("armor", 500));
-	data.insert(std::make_pair("shoes", 750));
-    
-	for (auto iter = data.begin(); iter != data.end(); iter++)
+
+	std::set<int> data;
+	std::set<int>::iterator iter;
+
+	data.insert(10);
+	data.insert(80);
+	data.insert(35);
+	data.insert(40);
+	data.insert(50);
+
+	for (iter = data.begin(); iter != data.end(); iter++)
 	{
-		std::cout << "Key : " << iter->first << std::endl;
-		std::cout << "Value : " << iter->second << std::endl;
+		std::cout << *iter << std::endl;
+
 	}
 
-	std::cout << "검색한 Key의 값 : " << data.find("shoes")->second << std::endl;
-	data.erase("shoes");
+	// [10] [35] [40] [50] [80]
 
-	for (auto iter = data.begin(); iter != data.end(); iter++)
+	iter = data.find(77);
+
+	if (iter != data.end())
 	{
-		std::cout << "Key : " << iter->first << std::endl;
-		std::cout << "Value : " << iter->second << std::endl;
+		std::cout << "데이터가 존재합니다." << std::endl;
+	}
+	else
+	{
+		std::cout << "데이터가 없습니다." << std::endl;
 	}
 	*/
 
-	// 알파벳 갯수 
-	// 각 알파벳이 단어에 몇 개가 포함되어 있는지 구하는 프로그램입니다.
+	// 함수의 오버로딩
+	/*
+	   다른 매개변수들을 가진 같은 이름의 함수를 여러 개 정의할 수 있습니다.
 	
-	// 예제 입력 -> baekjoon
-	// 
-	//              a b c d e f g h i j k l m n o p q r s t u v w x y z
-	// 예제 출력 -> 1 1 0 0 1 0 0 0 0 1 1 0 0 1 2 0 0 0 0 0 0 0 0 0 0 0
+	   Function('G');
+	   Function(10, 20);
 
-	std::string value;
-  
-	std::getline(std::cin, value);
+	   Phone iPhone(10,20);
+	*/
 
+
+	// O X 퀴즈
+	// "OOXXOXXOOO와 같은 OX퀴즈의 결과가 있습니다."
+	// O는 문제를 맞춘 것이고, X는 틀린 것입니다.
+    // 문제를 맞은 경우는 그 문제의 점수는 그 문제까지 연속된 O의 개수가 됩니다.
+	// "OOXXOXXOOO" = 1 + 2 + 0 + 0 + 1 + 0 + 0 + 1 + 2 + 3 = 10
+
+	// 입력받을 문자배열을 선언하고
+	// 원하는 OX를 입력하세요
+	/*
 	int result = 0;
+	std::string input;
+	int count = 1;
 
-	for (int i = 97; i <= 122; i++)
+	std::getline(std::cin, input);
+
+	for (int i = 0; i < input.length(); i++)
 	{
-		for (int j = 0; j < value.length(); j++)
+		if (input[i] == 'O')
 		{
-			if (value[j] == (char)i)
-			{
-				result++; 
-			}
+			result += count;
+			count++;
 		}
-
-		std::cout << result << std::endl;
-		result = 0;
+		
+		if (input[i] == 'X')
+		{
+			count = 1;
+		}
 	}
-	
+
+	std::cout << result << std::endl;
+	*/
+
 
 	return 0;
 }
